2addcbf3eb51f5a708c87b869107ea6afe38d7eb
Steve Wills <steve@mouf.net>
2019-07-31 22:11:21 -0400

    cleanup

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-        "github.com/docker/machine/libmachine/drivers/plugin"
-        "gitlab.mouf.net/swills/docker-machine-driver-bhyve/bhyve"
+	"github.com/docker/machine/libmachine/drivers/plugin"
+	"gitlab.mouf.net/swills/docker-machine-driver-bhyve/bhyve"
 )
 
 func main() {
-        plugin.RegisterDriver(new(bhyve.Driver))
+	plugin.RegisterDriver(new(bhyve.Driver))
 }
-
